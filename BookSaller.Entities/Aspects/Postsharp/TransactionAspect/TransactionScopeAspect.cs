ab8a9eada7c706f79ffdd353b8da61aa8f4e8350
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-06 21:39:36 +0300

    ClientSideValidation

---
+++ BookSaller.Entities/Aspects/Postsharp/TransactionAspect/TransactionScopeAspect.cs
@@ -0,0 +1,21 @@
+﻿using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSaller.Entities.Aspects.Postsharp.TransactionAspect
+{
+    [PSerializable]
+    public class TransactionScopeAspect:OnMethodBoundaryAspect
+    {
+        public TransactionScopeAspect()
+        {
+
+        }
+
+
+    }
+}
---
+++ BookSaller.Entities/Aspects/Postsharp/ValidationAspect/FluentValidationAspect.cs
@@ -0,0 +1,35 @@
+﻿using BookSaller.Entities.Utilities;
+using FluentValidation;
+using PostSharp.Aspects;
+using PostSharp.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSaller.Entities.Aspects.Postsharp.FluentValidationAspect
+{
+    [PSerializable]
+    public class FluentValidationAspect : OnMethodBoundaryAspect
+    {
+        Type _validationType;
+        public FluentValidationAspect(Type validationType)
+        {
+            _validationType = validationType;
+        }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validationType);
+            var entityType = _validationType.BaseType.GetGenericArguments()[0];
+            var entities = args.Arguments.Where(t => t.GetType() == entityType);
+
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator,entity);
+            }
+            
+        }
+    }
+}
