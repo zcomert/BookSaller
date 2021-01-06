e6d5462841863b9af0798820f2e8d3dc4406025e
Zafer CÖMERT <zcomert@samsun.edu.tr>
2021-01-06 21:39:36 +0300

    ClientSideValidation

---
+++ BookSaller.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,20 @@
+﻿using BookSaller.Entities.Concrete;
+using BookSaller.Entities.ValidationRules.FluentValidation;
+using FluentValidation;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSaller.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Book>>().To<BookValidator>().InSingletonScope();
+        }
+    }
+}
